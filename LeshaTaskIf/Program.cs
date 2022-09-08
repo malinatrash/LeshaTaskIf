3bf5051f14dbab5d75ebc55149ca6d23697d23c9
malinatrash <malinatrashh@gmail.com>
2022-09-08 23:14:20 +0800

    Initial commit

---
+++ LeshaTaskIf/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace LeshaTaskIf
+{
+    public class Logic
+    {
+        public static int Calculate(int number)
+        {
+            var maxNumber = number / 10 % 10;
+
+            if (number / 100 > maxNumber)
+            {
+                maxNumber = number / 100;
+            }
+            if (number % 10 > maxNumber)
+            {
+                maxNumber = number % 10;
+            }
+
+            return (maxNumber);
+        }
+        
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var anyNumber = int.Parse(Console.ReadLine());
+
+            var response = Logic.Calculate(anyNumber);
+            Console.WriteLine(response);
+        }
+    }
+}
---
+++ TestProject1/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LeshaTaskIf;
+using NUnit.Framework;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FirstNumberBiggest()
+        {
+            var number = 321;
+            var response = Logic.Calculate(number);
+            Assert.AreEqual(3, response);
+        }
+        [Test]
+        public void SecondNumberBiggest()
+        {
+            var number = 231;
+            var response = Logic.Calculate(number);
+            Assert.AreEqual(3, response);
+        }
+        [Test]
+        public void ThirdNumberBiggest()
+        {
+            var number = 123;
+            var response = Logic.Calculate(number);
+            Assert.AreEqual(3, response);
+        }
+    }
+}
